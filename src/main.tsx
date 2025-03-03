e1e7c46cc70f97bee03c5f498f2f52d6b498624c
Vennis-F <hoanganhgo28062001@gmail.com>
2025-03-03 18:57:47 +0700

    Feat: Add analystics vercel

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react';
-import store, { persistor } from './store/store';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import store, { persistor } from "./store/store";
+import { Analytics } from "@vercel/analytics/react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
+        <Analytics />
       </PersistGate>
     </Provider>
-
   </React.StrictMode>
-)
+);
